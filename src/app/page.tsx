26359081035c0f405f652ff251f6b34d5c82730f
Andriikryl <andreyk557@gmail.com>
2024-07-08 15:10:00 +0300

    add wrapper to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Todo } from "@/components/todo/Todo";
+import { HabitsTasks } from "@/components/habitsTasks/HabitsTasks";
+import { TasksWrapper } from "@/components/tsaksWrapper/TasksWrapper";
 
 export default function Home() {
-  return <Todo />;
+  return (
+    <>
+      <TasksWrapper>
+        <HabitsTasks />
+        <Todo />
+      </TasksWrapper>
+    </>
+  );
 }
