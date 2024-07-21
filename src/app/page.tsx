5306159de93825a081b8419e43d3e5ce30e383ed
Andriikryl <andreyk557@gmail.com>
2024-07-21 15:31:15 +0300

    add dailies to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import styles from "./page.module.css";
 import { Todo } from "@/components/todo/Todo";
 import { HabitsTasks } from "@/components/habitsTasks/HabitsTasks";
 import { TasksWrapper } from "@/components/tsaksWrapper/TasksWrapper";
+import { Dailies } from "@/components/dailies/Dailies";
 
 export default function Home() {
   return (
     <>
       <TasksWrapper>
         <HabitsTasks />
+        <Dailies />
         <Todo />
       </TasksWrapper>
     </>
