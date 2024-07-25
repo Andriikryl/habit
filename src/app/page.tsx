20645a8ecfd2153194d6b03bbc48a9b2c38c06de
Andriikryl <andreyk557@gmail.com>
2024-07-25 13:38:24 +0300

    add home flow frapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import { Todo } from "@/components/todo/Todo";
 import { HabitsTasks } from "@/components/habitsTasks/HabitsTasks";
 import { TasksWrapper } from "@/components/tsaksWrapper/TasksWrapper";
 import { Dailies } from "@/components/dailies/Dailies";
+import { Statistics } from "@/components/statistics/Statistics";
 
 export default function Home() {
   return (
-    <>
+    <div className="home__flow">
+      <Statistics />
       <TasksWrapper>
         <HabitsTasks />
         <Dailies />
         <Todo />
       </TasksWrapper>
-    </>
+    </div>
   );
 }
